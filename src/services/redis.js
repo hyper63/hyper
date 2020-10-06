6c3b4fbbab6dd774b4a80066758d1acc622e98ea
Tom Wilson <twilson63@gmail.com>
2020-10-06 17:40:45 -0400

    added redis test

--- src/services/redis.js
+++ src/services/redis.js
@@ -6,25 +6,23 @@ const noop = () => Async.Resolved({ ok: true });
 
 // atlas cache implementation interface
 module.exports = (env = { url: "redis://127.0.0.1:6379" }) => {
-  const redis = new YoRedis({ url: env.url });
-  const call = Async.fromPromise(redis.call);
+  const redis = new YoRedis(env);
+  const call = Async.fromPromise(redis.call.bind(redis));
   const createKey = (store, key) => `${store}_${key}`;
   // todo add ttl support
   const set = ({ store, key, value, ttl }) =>
-    call("set", createKey(store, key), JSON.stringify(value));
+    call("set", createKey(store, key), JSON.stringify(value)).map(() => ({
+      ok: true,
+    }));
   return {
     createStore: noop,
     destroyStore: noop,
-    createDoc: noop,
-    getDoc: noop,
-    updateDoc: noop,
-    deleteDoc: noop,
-    /*
     createDoc: set,
-    getDoc: async ({ store, key }) =>
-      JSON.parse(await call("get", createKey(store, key))),
+    getDoc: ({ store, key }) =>
+      call("get", createKey(store, key)).map((v) => JSON.parse(v)),
     updateDoc: set,
-    deleteDoc: ({ store, key }) => call("del", createKey(store, key)),
-    */
+    deleteDoc: ({ store, key }) =>
+      call("del", createKey(store, key)).map(() => ({ ok: true })),
+    close: () => redis.end(),
   };
 };
