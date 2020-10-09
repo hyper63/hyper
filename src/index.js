d464c9462ec4d4edf76c9434053f102ded5e4db1
Tom Wilson <twilson63@gmail.com>
2020-10-09 14:39:53 -0400

    updated swagger document

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const helmet = require("helmet");
 const core = require("./bind")();
 
@@ -14,7 +15,7 @@ const bindCore = (req, res, next) => {
 };
 
 app.use(helmet());
-
+app.use(cors({ credentials: true }));
 //app.use("/micro/data", require("./api/data"));
 
 // cache api
