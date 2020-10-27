3333fc4393dd1b7e204679c0c6f0f452c63fb6c2
Tom Wilson <twilson63@gmail.com>
2020-10-27 15:14:59 -0400

    updated launcher with some couch config

--- launcher/index.js
+++ launcher/index.js
@@ -1,18 +1,20 @@
-const sh = require('shelljs')
-const fs = require('fs')
-const path = require('path')
-const child_process = require('child_process')
+const sh = require("shelljs");
+const fs = require("fs");
+const path = require("path");
+const child_process = require("child_process");
 
 module.exports = () => {
+  if (!sh.which("docker-compose")) {
+    sh.echo(
+      "Sorry, this script requires docker-compose which can be installed from docker desktop"
+    );
+    sh.exit(1);
+  }
+  if (!sh.test("-e", "~/.hyper63")) {
+    sh.mkdir("~/.hyper63");
+  }
 
-if (!sh.which('docker-compose')) {
-  sh.echo('Sorry, this script requires docker-compose which can be installed from docker desktop')
-  sh.exit(1)
-}
-
-sh.mkdir('~/.hyper63')
-
-const config = `version: "3.8"
+  const config = `version: "3.8"
 services:
   web:
     image: "hyper63/hyper63:unstable"
@@ -45,17 +47,24 @@ services:
       ports:
         - "9000:9000"
       command: server /data
-`
+`;
 
-fs.writeFileSync(`${process.env.HOME}/.hyper63/docker-compose.yml`, config)
+  fs.writeFileSync(`${process.env.HOME}/.hyper63/docker-compose.yml`, config);
 
-sh.cd('~/.hyper63')
-sh.exec('docker-compose up -d')
+  sh.cd("~/.hyper63");
+  sh.exec("docker-compose up -d");
 
-setTimeout(() => {
-
-sh.exec(`docker exec hyper63_couchdb_1 curl -X POST -H "Content-Type: application/json" localhost:5984/_cluster_setup -d '{"action":"enable_single_node", "bind_address":"0.0.0.0"}' -u 'admin:password' `)
-
-}, 2000)
-
-}
+  setTimeout(() => {
+    sh.exec(
+      `docker exec hyper63_couchdb_1 curl -X POST -H "Content-Type: application/json" localhost:5984/_cluster_setup -d '{"action":"enable_single_node", "bind_address":"0.0.0.0"}' -u 'admin:password' `,
+      { silent: true },
+      (code, stdout, stderr) => {
+        if (code === 0) {
+          console.log("Successfully setup database");
+        } else {
+          console.log("ERROR! Could not setup database, try to re-run script");
+        }
+      }
+    );
+  }, 5000);
+};
