e17876f1297f031c19991b351b18b6acf3fcb57d
Tom Wilson <twilson63@gmail.com>
2020-11-09 17:21:13 -0500

    feat(docs): create docs for adapters

--- docs/src/components/BlogTeaser.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let blog;
-  export let helpers;
-</script>
-
-<div class="entry">
-  <a href={helpers.permalinks.blog({ slug: blog.slug })}>{blog.frontmatter.title}</a>
-  <p>{blog.frontmatter.excerpt}</p>
-</div>
---
+++ docs/src/routes/index.svelte
@@ -0,0 +1,34 @@
+<style>
+	h1, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>hyper63 documentation</title>
+</svelte:head>
+
+<h1>hyper63</h1>
+
+<p><strong>A cloud agnostic service gateway.</strong></p>
+
+
+<a href="/documentation">View Documentation</a>
