55efb8f14d5ecd2a0590d52c4ae2f45bf79f611a
Tom Wilson <twilson63@gmail.com>
2021-01-22 14:03:54 -0500

    added handler for error status codes

--- packages/app-express/api/utils.js
+++ packages/app-express/api/utils.js
@@ -2,7 +2,7 @@ exports.fork = (res, code, m) =>
   m.fork(
     (error) => {
       if (error.status) {
-        return res.status(error.status).send(error.message)
+        return res.status(error.status).send({ok: false, msg: error.message})
       } 
       res.status(500).send(error)
     },
