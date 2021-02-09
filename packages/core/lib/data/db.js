c7b45bbab6cdbcf8361951319681800390acc103
Tom Wilson <twilson63@gmail.com>
2021-02-09 21:58:15 +0000

    update core with bulk data method

--- packages/core/lib/data/db.js
+++ packages/core/lib/data/db.js
@@ -16,21 +16,26 @@ exports.remove = (name) =>
     .chain(triggerEvent('DATA:DELETE_DB'))
     .chain(apply("removeDatabase"));
 
-exports.query = (db, query) => 
-  of({db, query})
+exports.query = (db, query) =>
+  of({ db, query })
     .chain(apply("queryDocuments"))
     .chain(triggerEvent('DATA:QUERY'))
 
-exports.index = (db, name, fields) => 
-  of({db, name, fields})
+exports.index = (db, name, fields) =>
+  of({ db, name, fields })
     .chain(apply("indexDocuments"))
     .chain(triggerEvent('DATA:INDEX'))
 
-exports.list = (db, options) => 
-  of({db, ...options})
+exports.list = (db, options) =>
+  of({ db, ...options })
     .chain(apply("listDocuments"))
     .chain(triggerEvent('DATA:LIST'))
-    
+
+exports.bulk = (db, docs) =>
+  of({ db, docs })
+    .chain(apply("bulkDocuments"))
+    .chain(triggerEvent('DATA:BULK'))
+
 function validDbName(name) {
   return true;
 }
--- packages/core/lib/data/db_test.js
+++ packages/core/lib/data/db_test.js
@@ -8,6 +8,16 @@ const mockDb = {
   removeDatabase(name) {
     return Promise.resolve({ ok: true });
   },
+  bulkDocuments({ db, docs }) {
+    if (docs.length === 2) {
+      return Promise.resolve({
+        ok: true,
+        results: [{ ok: true, id: '1' }, { ok: true, id: '2' }]
+      })
+    } else {
+      return Promise.reject({ ok: false })
+    }
+  }
 };
 
 const fork = (m) => (t) => {
@@ -17,6 +27,13 @@ const fork = (m) => (t) => {
     () => t.ok(true)
   );
 };
+const handleFail = (m) => (t) => {
+  t.plan(1);
+  return m.fork(
+    () => t.ok(true),
+    () => t.ok(false)
+  );
+};
 
 const events = {
   dispatch: () => null
@@ -24,5 +41,7 @@ const events = {
 
 test("create database", fork(db.create("foo").runWith({ svc: mockDb, events })));
 test("remove database", fork(db.remove("foo").runWith({ svc: mockDb, events })));
+test('bulk documents', fork(db.bulk('foo', [{ id: '1' }, { id: '2' }]).runWith({ svc: mockDb, events })));
+test('bulk docs failure', handleFail(db.bulk('foo', []).runWith({ svc: mockDb, events })));
 //test("query database");
 //test("index database");
