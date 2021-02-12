f54618ca27d649adb5fc2011855ab30c0bf7cd67
Tyler Hall <0tillathehun0@gmail.com>
2021-02-12 14:55:24 -0500

    test(adapter-elasticsearch): entrypoint tests

--- packages/adapter-elasticsearch/index_test.js
+++ packages/adapter-elasticsearch/index_test.js
@@ -1,16 +1,43 @@
-const test = require('tape')
-const searchAdapter = require('./index')
 
-// TODO: fix this broken require
-const schema = require('../../utils/plugin-schema')
+const test = require('tape')
+const elasticsearchAdapterFactory = require('./index')
 
 test('validate adapter', t => {
-  t.plan(1)
-  try {
-    const result = schema(searchAdapter())
-    t.ok(true)
-  } catch (e) {
-    console.log(e)
-    t.ok(false)
+  const adapter = elasticsearchAdapterFactory({})
+  t.ok(adapter)
+  t.end()
+})
+
+test('validate load()', t => {
+  const config = { foo: 'bar' }
+  const adapter = elasticsearchAdapterFactory(config)
+
+  const loadedConfig = adapter.load({ fizz: 'buzz' })
+  t.equals(loadedConfig.foo, config.foo)
+  t.equals(loadedConfig.fizz, 'buzz')
+  t.end()
+})
+
+test('validate link()', t => {
+  const config = {
+    username: 'foo',
+    password: 'bar',
+    url: 'http://localhost:9200'
   }
+
+  const adapter = elasticsearchAdapterFactory(config)
+  t.ok(adapter.link(config)())
+  t.end()
+})
+
+test('validate link() - no url', t => {
+  const config = {
+    username: 'foo',
+    password: 'bar',
+    no_url: 'http://localhost:9200'
+  }
+
+  const adapter = elasticsearchAdapterFactory(config)
+  t.throws(() => adapter.link(config)(), 'Config URL is required elastic search')
+  t.end()
 })
