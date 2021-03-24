cc6d9d55115a02a266babed388559a0c1bed4222
Tom Wilson <twilson63@gmail.com>
2021-03-24 15:46:33 -0400

    added beequeue to micro

--- images/micro/hyper63.config.js
+++ images/micro/hyper63.config.js
@@ -3,6 +3,7 @@ import couchdb from '@hyper63/adapter-couchdb'
 import redis from '@hyper63/adapter-redis'
 import minio from '@hyper63/adapter-minio'
 import es from '@hyper63/adapter-elasticsearch'
+import bq from '@hyper63/adapter-beequeue'
 
 export default {
   app: express,
@@ -10,6 +11,7 @@ export default {
     { port: 'data', plugins:[couchdb({url: 'http://admin:password@0.0.0.0:5984'})]},
     { port: 'cache', plugins:[redis({url: 'redis://0.0.0.0:6379'})]},
     { port: 'storage', plugins:[minio({url: 'http://admin:password@0.0.0.0:9000'})]},
-    { port: 'search', plugins:[es({url: 'http://0.0.0.0:9200'})]}
+    { port: 'search', plugins:[es({url: 'http://0.0.0.0:9200'})]},
+    { port: 'queue', plugins:[bq.default({redis: 'redis://0.0.0.0:6379'})]}
   ]
 }
