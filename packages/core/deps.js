f6748c3c45252a3c59029b55e9d3b15aec5e6b58
Tyler Hall <tyler@hyper.io>
2021-06-28 22:04:13 -0400

    fix(core): pull ports from nest.land

--- packages/core/deps.js
+++ packages/core/deps.js
@@ -6,3 +6,10 @@ export * as ms from "https://cdn.skypack.dev/ms@2.1.3";
 export { cuid } from "https://deno.land/x/cuid@v1.0.0/index.js";
 export { join } from "https://deno.land/std@0.99.0/path/mod.ts";
 export { exists } from "https://deno.land/std@0.99.0/fs/mod.ts";
+
+export { queue } from "https://x.nest.land/hyper-port-queue@0.1.4/mod.js";
+export { cache } from "https://x.nest.land/hyper-port-cache@1.1.4/mod.js";
+export { data } from "https://x.nest.land/hyper-port-data@1.0.11/mod.js";
+export { storage } from "https://x.nest.land/hyper-port-storage@1.0.2/mod.js";
+export { search } from "https://x.nest.land/hyper-port-search@1.0.4/mod.js";
+export { hooks } from "https://x.nest.land/hyper-port-hooks@1.0.2/mod.js";
