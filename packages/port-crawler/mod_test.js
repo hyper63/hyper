35ac00b017af3aedbd673fc6e08728493a63472c
Tom Wilson <twilson63@gmail.com>
2021-07-10 13:03:37 +0000

    feat: implement port schema and test upsert (#263)

--- packages/port-crawler/mod_test.js
+++ packages/port-crawler/mod_test.js
@@ -4,7 +4,34 @@ import { crawler as crawlerPort } from "./mod.js";
 import { assertEquals } from "./dev_deps.js";
 
 const test = Deno.test;
+const crawlerJob = {
+  app: "app",
+  name: "name",
+  source: "https://example.com",
+  script: "",
+  depth: 1,
+  target: {
+    url: "https://target.com",
+    secret: "secret",
+    sub: "sub",
+  },
+  notify: "https://notify.com",
+};
 
-test("first test", () => {
-  assertEquals(true, true);
+const adapter = {
+  upsert: (o) => Promise.resolve({ ok: true }),
+  get: (o) =>
+    Promise.resolve({
+      id: "app-name",
+      ...crawlerJob,
+    }),
+  start: (o) => Promise.resolve({ ok: true }),
+  delete: (o) => Promise.resolve({ ok: true }),
+};
+
+const a = crawlerPort(adapter);
+
+test("create crawler", async () => {
+  const result = await a.upsert(crawlerJob);
+  assertEquals(result.ok, true);
 });
