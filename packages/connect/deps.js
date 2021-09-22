2fe1eed37c2b23efa69522a810082a57b491518a
Tom Wilson <twilson63@gmail.com>
2021-09-22 21:13:55 +0000

    bug (connect) refactored deps to resolve error with deno (#331)

--- packages/connect/deps.js
+++ packages/connect/deps.js
@@ -1,13 +1,20 @@
-let crocks, R, signJWT, jwt;
+let signJWT, jwt;
+
+const crocksLib = globalThis.Deno
+  ? "https://cdn.skypack.dev/crocks@v0.12.4"
+  : "crocks";
+const RLib = globalThis.Deno ? "https://cdn.skypack.dev/ramda" : "ramda";
+const jwtLib = globalThis.Deno
+  ? "https://deno.land/x/djwt@v2.1/mod.ts"
+  : "jsonwebtoken";
+
+const crocks = (await import(crocksLib)).default;
+const R = await import(RLib);
 
 if (globalThis.Deno) {
-  crocks = (await import("https://cdn.skypack.dev/crocks")).default;
-  R = await import("https://cdn.skypack.dev/ramda");
-  signJWT = (await import("https://deno.land/x/djwt@v2.1/mod.ts")).create;
+  signJWT = (await import(jwtLib)).create;
 } else {
-  crocks = (await import("crocks")).default;
-  R = (await import("ramda")).default;
-  jwt = (await import("jsonwebtoken"));
+  jwt = await import(jwtLib);
   signJWT = (headers, payload, secret) =>
     jwt(payload, secret, { algorithm: headers.alg });
 }
