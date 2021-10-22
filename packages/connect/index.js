f0774a1041227b523ddef728bc0e54a73d76d4c3
Tom Wilson <twilson63@gmail.com>
2021-10-22 20:26:09 +0000

    added errors to throw if Request and fetch is not specified! #360

--- packages/connect/index.js
+++ packages/connect/index.js
@@ -1,8 +1,14 @@
 import connect from "./nodejs/index.js";
 
 if (!globalThis.Request) {
-  globalThis.Request = (await import("node-fetch")).Request;
+  throw new Error(
+    "Request is not found, please import the Request Class from a fetch library like node-fetch",
+  );
 }
-if (!globalThis.fetch) globalThis.fetch = (await import("node-fetch")).default;
+if (!globalThis.fetch) {
+  throw new Error(
+    "fetch is not found, please import fetch function from a fetch library like node-fetch",
+  );
+}
 
 export default connect;
