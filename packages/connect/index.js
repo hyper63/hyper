f0f4942b7f85a354669363ae594f3dbe6f47e247
Tom Wilson <twilson63@gmail.com>
2021-10-22 20:17:16 +0000

    wrapped parenthesis around dynamic import #360

--- packages/connect/index.js
+++ packages/connect/index.js
@@ -1,6 +1,8 @@
 import connect from "./nodejs/index.js";
 
-if (!globalThis.Request) globalThis.Request = await import('node-fetch').Request;
-if (!globalThis.fetch) globalThis.fetch = await import('node-fetch').default;
+if (!globalThis.Request) {
+  globalThis.Request = (await import("node-fetch")).Request;
+}
+if (!globalThis.fetch) globalThis.fetch = (await import("node-fetch")).default;
 
 export default connect;
