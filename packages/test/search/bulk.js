a4aa4b69711d4e26ab0a63594544fdac5b96f540
Tom Wilson <tom@hyper.io>
2022-01-19 12:45:53 +0000

    fix(test): added search bulk test and upgraded hyper-connect #425

--- packages/test/search/bulk.js
+++ packages/test/search/bulk.js
@@ -3,32 +3,28 @@ import { $fetch } from "../lib/utils.js";
 import { assert, assertEquals } from "asserts";
 
 const docs = [{
-  id: '1',
-  type: 'movie',
-  title: 'Ghostbusters',
-  year: '1980'
+  id: "1",
+  type: "movie",
+  title: "Ghostbusters",
+  year: "1980",
 }, {
-  id: '2', 
-  type: 'movie',
-  title: 'Avengers',
-  year: '2011'
-}]
+  id: "2",
+  type: "movie",
+  title: "Avengers",
+  year: "2011",
+}];
 
 const test = Deno.test;
 
 export default function (search) {
-
-  test('POST /search/:index/_bulk', () => 
+  test("POST /search/:index/_bulk", () =>
     $fetch(() => search.load(docs))
-      .map(r => {
-        assert(r.ok)
-        assert(r.results[0].ok)
-        assert(r.results[1].ok)
-        assertEquals(r.results[0].id, "1")
-        assertEquals(r.results[1].id, "2")
-
+      .map((r) => {
+        assert(r.ok);
+        assert(r.results[0].ok);
+        assert(r.results[1].ok);
+        assertEquals(r.results[0].id, "1");
+        assertEquals(r.results[1].id, "2");
       })
-      
-      .toPromise()
-  )
+      .toPromise());
 }
