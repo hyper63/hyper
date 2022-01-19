569dc091d6d49be4668d12d96768eed562e3e092
Tom Wilson <tom@hyper.io>
2022-01-19 12:50:00 +0000

    chore(images/nano): update test script to support Deno v1.17.3

--- images/nano/scripts/compile.sh
+++ images/nano/scripts/compile.sh
@@ -1,1 +1,1 @@
-deno compile --allow-env --allow-read --allow-write=/tmp --allow-net --unstable --no-check --output hyper mod.js
+deno compile --allow-env --allow-read --allow-write=/tmp --allow-net --unstable --no-check=remote --output hyper mod.js
