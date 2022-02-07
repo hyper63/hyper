1155f2b10fa035cc81efcbba5303cfe7cb94fca5
Tyler Hall <tyler@hyper.io>
2022-02-07 19:49:55 +0000

    chore(app-opine): bump dependencies

--- packages/app-opine/dev_deps.js
+++ packages/app-opine/dev_deps.js
@@ -2,5 +2,5 @@ export {
   assert,
   assertEquals,
   assertObjectMatch,
-} from "https://deno.land/std@0.117.0/testing/asserts.ts";
-export { superdeno } from "https://deno.land/x/superdeno@4.7.1/mod.ts";
+} from "https://deno.land/std@0.125.0/testing/asserts.ts";
+export { superdeno } from "https://deno.land/x/superdeno@4.7.2/mod.ts";
