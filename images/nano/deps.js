b85b2feecc866fbfc9eb229713cf7fc5712d6a89
Tom Wilson <tom@hyper.io>
2022-02-07 16:21:27 +0000

    wip (hyper-test): setup nano image to test queue service #434

--- images/nano/deps.js
+++ images/nano/deps.js
@@ -6,8 +6,5 @@ export { default as dndb } from "https://x.nest.land/hyper-adapter-dndb@2.0.0/mo
 export { default as cache } from "https://x.nest.land/hyper-adapter-sqlite@1.0.0/mod.js";
 export { default as fs } from "https://x.nest.land/hyper-adapter-fs@1.0.8/mod.js";
 export { default as minisearch } from "https://x.nest.land/hyper-adapter-minisearch@1.1.1/mod.js";
-<<<<<<< HEAD
-=======
 // include queue locally
 export { default as queue } from "https://x.nest.land/hyper-adapter-queue@0.0.1/mod.js";
->>>>>>> 48a213004fc4bc9a15e35ff229762e0e520a3014
