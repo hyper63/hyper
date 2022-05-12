4094e495ca60a4b62aef47af028923d5a052a29c
Tyler Hall <tyler@hyper.io>
2022-05-12 19:22:40 +0000

    chore(connect): remove separate node code #509

--- packages/connect/src/services/info.ts
+++
@@ -1,8 +0,0 @@
-//import { Request } from "node-fetch";
-
-import { HyperRequestFunction, Method } from "../types";
-
-const service = "info" as const;
-
-export const services = () =>
-  (h: HyperRequestFunction) => h({ service, method: Method.GET });
