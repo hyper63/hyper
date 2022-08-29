1d9c0eb56c710a8f61ab7d020cca057e75622318
Tyler Hall <tyler@hyper.io>
2022-08-29 15:10:17 +0000

    chore(core): bump deps #519

--- packages/core/deps.js
+++ packages/core/deps.js
@@ -1,11 +1,11 @@
 export * as R from "https://cdn.skypack.dev/ramda@0.28.0";
 export { default as crocks } from "https://cdn.skypack.dev/crocks@0.12.4";
-export * as z from "https://cdn.skypack.dev/zod@3.12.1";
+export * as z from "https://cdn.skypack.dev/zod@3.18.0";
 export { ms } from "https://deno.land/x/ms@v0.1.0/ms.ts";
 
 export { cuid } from "https://deno.land/x/cuid@v1.0.0/index.js";
-export { join } from "https://deno.land/std@0.134.0/path/mod.ts";
-export { exists } from "https://deno.land/std@0.134.0/fs/mod.ts";
+export { join } from "https://deno.land/std@0.153.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.153.0/fs/mod.ts";
 
 export {
   HyperErr,
@@ -16,7 +16,7 @@ export {
 export { queue } from "https://x.nest.land/hyper-port-queue@0.2.0/mod.js";
 export { cache } from "https://x.nest.land/hyper-port-cache@1.2.0/mod.js";
 export { data } from "https://x.nest.land/hyper-port-data@1.2.1/mod.js";
-export { storage } from "https://x.nest.land/hyper-port-storage@1.2.0/mod.js";
+export { storage } from "https://x.nest.land/hyper-port-storage@1.3.0/mod.js";
 export { search } from "https://x.nest.land/hyper-port-search@1.1.0/mod.js";
 export { hooks } from "https://x.nest.land/hyper-port-hooks@1.1.0/mod.js";
 export { crawler } from "https://x.nest.land/hyper-port-crawler@0.0.4/mod.js";
