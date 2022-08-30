c42a749bb579f9d25d2ef6f9e1f6ad56e2cea4cd
Tyler Hall <tyler@hyper.io>
2022-08-30 16:27:57 +0000

    style(connect): deno fmt

--- packages/connect/deno/services/queue.ts
+++ packages/connect/deno/services/queue.ts
@@ -2,13 +2,11 @@ import { HyperRequestFunction, Method, QueueStatus } from "../types.ts";
 
 const service = "queue" as const;
 
-export const enqueue = (body: unknown) =>
-  (h: HyperRequestFunction) => h({ service, method: Method.POST, body });
+export const enqueue = (body: unknown) => (h: HyperRequestFunction) =>
+  h({ service, method: Method.POST, body });
 
-export const errors = () =>
-  (h: HyperRequestFunction) =>
-    h({ service, method: Method.GET, params: { status: QueueStatus.ERROR } });
+export const errors = () => (h: HyperRequestFunction) =>
+  h({ service, method: Method.GET, params: { status: QueueStatus.ERROR } });
 
-export const queued = () =>
-  (h: HyperRequestFunction) =>
-    h({ service, method: Method.GET, params: { status: QueueStatus.READY } });
+export const queued = () => (h: HyperRequestFunction) =>
+  h({ service, method: Method.GET, params: { status: QueueStatus.READY } });
--- packages/connect/deno/utils/hyper-request.ts
+++ packages/connect/deno/utils/hyper-request.ts
@@ -15,45 +15,45 @@ interface HyperRequestParams {
 }
 
 export const hyper = (conn: URL, domain: string) =>
-  async (
-    { service, method, resource, body, params, action }: HyperRequest,
-  ): Promise<HyperRequestParams> => {
-    const isCloud = /^cloud/.test(conn.protocol);
-    const protocol = isCloud ? "https:" : conn.protocol;
+async (
+  { service, method, resource, body, params, action }: HyperRequest,
+): Promise<HyperRequestParams> => {
+  const isCloud = /^cloud/.test(conn.protocol);
+  const protocol = isCloud ? "https:" : conn.protocol;
 
-    let options = {
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-      method: method ? method : Method.GET,
-    } as RequestOptions;
+  let options = {
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+    method: method ? method : Method.GET,
+  } as RequestOptions;
 
-    if (body) {
-      options = assoc("body", JSON.stringify(body), options) as RequestOptions;
-    }
+  if (body) {
+    options = assoc("body", JSON.stringify(body), options) as RequestOptions;
+  }
 
-    if (conn.username && conn.password) {
-      const token = await generateToken(conn.username, conn.password);
-      options.headers = new Headers({
-        ...Object.fromEntries(options.headers.entries()),
-        Authorization: `Bearer ${token}`,
-      });
-    }
-    const pathname = isCloud ? conn.pathname : "";
-    const appdomain = isCloud ? "/" + domain : conn.pathname;
+  if (conn.username && conn.password) {
+    const token = await generateToken(conn.username, conn.password);
+    options.headers = new Headers({
+      ...Object.fromEntries(options.headers.entries()),
+      Authorization: `Bearer ${token}`,
+    });
+  }
+  const pathname = isCloud ? conn.pathname : "";
+  const appdomain = isCloud ? "/" + domain : conn.pathname;
 
-    let url = `${protocol}//${conn.host}${pathname}/${service}${appdomain}`;
+  let url = `${protocol}//${conn.host}${pathname}/${service}${appdomain}`;
 
-    if (service === "info") {
-      url = `${protocol}//${conn.host}`;
-    }
+  if (service === "info") {
+    url = `${protocol}//${conn.host}`;
+  }
 
-    if (resource) url += `/${resource}`;
-    else if (action) url += `/${action}`;
+  if (resource) url += `/${resource}`;
+  else if (action) url += `/${action}`;
 
-    if (params) {
-      url += `?${new URLSearchParams(params).toString()}`;
-    }
+  if (params) {
+    url += `?${new URLSearchParams(params).toString()}`;
+  }
 
-    return { url, options };
-  };
+  return { url, options };
+};
