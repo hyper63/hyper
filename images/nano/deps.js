5eaccffb3fecc39fc82dc8a34668ce07e37dcde0
Tyler Hall <tyler@hyper.io>
2022-09-02 14:15:53 +0000

    chore(nano): bump deps

--- images/nano/deps.js
+++ images/nano/deps.js
@@ -2,7 +2,7 @@ export { default as hyper } from "https://x.nest.land/hyper@3.3.0/mod.js";
 export { default as app } from "https://x.nest.land/hyper-app-opine@2.2.0/mod.js";
 
 // adapters
-export { default as pouchdb } from "https://x.nest.land/hyper-adapter-pouchdb@0.1.5/mod.js";
+export { default as pouchdb } from "https://x.nest.land/hyper-adapter-pouchdb@0.1.6/mod.js";
 export { default as cache } from "https://x.nest.land/hyper-adapter-sqlite@2.0.1/mod.js";
 export { default as fs } from "https://x.nest.land/hyper-adapter-fs@2.1.0/mod.js";
 export { default as minisearch } from "https://x.nest.land/hyper-adapter-minisearch@2.0.0/mod.js";
