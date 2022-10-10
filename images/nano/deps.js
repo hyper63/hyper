6449890115f5858c8022a328333e03627c051c4a
Tyler Hall <tyler@hyper.io>
2022-10-10 16:31:38 +0000

    chore(nano): bump deps

--- images/nano/deps.js
+++ images/nano/deps.js
@@ -3,12 +3,12 @@ export { default as app } from "https://x.nest.land/hyper-app-opine@2.2.0/mod.js
 
 // adapters
 export { default as pouchdb } from "https://x.nest.land/hyper-adapter-pouchdb@0.1.6/mod.js";
-export { default as cache } from "https://x.nest.land/hyper-adapter-sqlite@2.0.1/mod.js";
+export { default as cache } from "https://x.nest.land/hyper-adapter-sqlite@2.0.2/mod.js";
 export { default as fs } from "https://x.nest.land/hyper-adapter-fs@2.1.0/mod.js";
 export { default as minisearch } from "https://x.nest.land/hyper-adapter-minisearch@2.0.0/mod.js";
-export { default as queue } from "https://x.nest.land/hyper-adapter-queue@0.0.5/mod.js";
+export { default as queue } from "https://x.nest.land/hyper-adapter-queue@0.1.0/mod.js";
 
-export { join } from "https://deno.land/std@0.153.0/path/mod.ts";
-export { parse } from "https://deno.land/std@0.153.0/flags/mod.ts";
-export { exists } from "https://deno.land/std@0.153.0/fs/mod.ts";
-export * as Colors from "https://deno.land/std@0.153.0/fmt/colors.ts";
+export { join } from "https://deno.land/std@0.159.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.159.0/flags/mod.ts";
+export { exists } from "https://deno.land/std@0.159.0/fs/mod.ts";
+export * as Colors from "https://deno.land/std@0.159.0/fmt/colors.ts";
