cd3ecb5d0fe85d4e1e82f65a19e16838fb533334
Tyler Hall <tyler@hyper.io>
2022-12-12 20:51:19 +0000

    chore(app-opine): bump deps and refactor usages

--- packages/app-opine/dev_deps.js
+++ packages/app-opine/dev_deps.js
@@ -2,5 +2,5 @@ export {
   assert,
   assertEquals,
   assertObjectMatch,
-} from "https://deno.land/std@0.152.0/testing/asserts.ts";
+} from "https://deno.land/std@0.167.0/testing/asserts.ts";
 export { superdeno } from "https://deno.land/x/superdeno@4.8.0/mod.ts";
