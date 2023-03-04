9fe6b695707a9461ea2710f5950e6542f6f31b33
Tyler Hall <tyler@hyper.io>
2023-03-04 16:54:38 +0000

    chore(app-opine): bump deps

--- packages/app-opine/deps.js
+++ packages/app-opine/deps.js
@@ -1,12 +1,12 @@
 export { json, opine, Router } from 'https://deno.land/x/opine@2.3.3/mod.ts'
 export { opineCors as cors } from 'https://deno.land/x/cors@v1.2.2/mod.ts'
 
-export { contentType as getMimeType } from 'https://deno.land/std@0.167.0/media_types/mod.ts'
 // TODO: refactor off deprecated: see https://github.com/denoland/deno_std/issues/1778
 export { MultipartReader } from 'https://deno.land/std@0.133.0/mime/mod.ts'
-export { Buffer } from 'https://deno.land/std@0.167.0/io/buffer.ts'
+export { contentType as getMimeType } from 'https://deno.land/std@0.178.0/media_types/mod.ts'
+export { Buffer } from 'https://deno.land/std@0.178.0/io/buffer.ts'
 
-export { default as helmet } from 'https://cdn.skypack.dev/helmet@5.0.2'
+export { default as helmet } from 'https://cdn.skypack.dev/helmet@5.1.1'
 export * as R from 'https://cdn.skypack.dev/ramda@0.28.0'
 export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
 
