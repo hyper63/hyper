ee5f5220dcb6f6fc2bcfc47a53fb5b40ada79404
Tyler Hall <tyler@hyper.io>
2023-03-05 01:40:45 +0000

    feat(app-express): add middleware and utils #556

---
+++ packages/app-express/utils.test.ts
@@ -0,0 +1,116 @@
+// deno-lint-ignore-file no-explicit-any ban-ts-comment
+import { crocks } from './deps.ts'
+import { assert, assertEquals } from './dev_deps.ts'
+
+import { fork, isFile, isMultipartFormData, isTrue } from './utils.ts'
+
+const { Async } = crocks
+
+Deno.test('utils', async (t) => {
+  await t.step('fork', async (t) => {
+    let result: any
+    const res: any = {
+      status: () => res,
+      send: (r: any) => {
+        result = r
+      },
+    }
+
+    await t.step(
+      'should set the status and response correctly',
+      async () => {},
+    )
+
+    await t.step('sanitize', async (t) => {
+      const env = Deno.env.get('DENO_ENV')
+      const cleanup = () => env ? Deno.env.set('DENO_ENV', env) : Deno.env.delete('DENO_ENV')
+
+      await t.step('should sanitize errors on both branches', async () => {
+        Deno.env.set('DENO_ENV', 'production')
+
+        // resolved success
+        await fork(res, 200, Async.Resolved({ ok: true }))
+        assert(result.ok)
+
+        // resolved error
+        await fork(
+          res,
+          200,
+          Async.Resolved({ ok: false, originalErr: 'foobar' }),
+        )
+        assertEquals(result.ok, false)
+        assert(!result.originalErr)
+
+        // rejected error (fatal)
+        await fork(
+          res,
+          200,
+          Async.Rejected({ ok: false, originalErr: 'foobar' }),
+        )
+        assertEquals(result, 'Internal Server Error')
+
+        cleanup()
+      })
+
+      await t.step('should NOT sanitize errors on both branches', async () => {
+        Deno.env.set('DENO_ENV', 'foo')
+
+        // resolved success
+        await fork(res, 200, Async.Resolved({ ok: true }))
+        assert(result.ok)
+
+        // resolved error
+        await fork(
+          res,
+          200,
+          Async.Resolved({ ok: false, originalErr: 'foobar' }),
+        )
+        assertEquals(result.ok, false)
+        assert(result.originalErr)
+
+        // rejected error (fatal)
+        await fork(
+          res,
+          200,
+          Async.Rejected({ ok: false, originalErr: 'foobar' }),
+        )
+        assertEquals(result.ok, false)
+        assert(result.originalErr)
+
+        cleanup()
+      })
+    })
+  })
+
+  await t.step('isMultipartFormData', async (t) => {
+    await t.step(
+      'should return true if header indicates multipart/form-data',
+      () => {
+        assert(isMultipartFormData('multipart/form-data'))
+        assert(!isMultipartFormData('application/json'))
+        // @ts-ignore
+        assert(!isMultipartFormData())
+      },
+    )
+  })
+
+  await t.step('isFile', async (t) => {
+    await t.step('should return true if path points to a file', () => {
+      assert(isFile('/foo.jpg'))
+      assert(!isFile('/foo'))
+      // @ts-ignore
+      assert(!isFile())
+    })
+  })
+
+  await t.step('isTrue', async (t) => {
+    await t.step('should return true if value is true-like', () => {
+      assert(isTrue(true))
+      assert(isTrue('true'))
+      assert(!isTrue('t'))
+      assert(!isTrue(false))
+      assert(!isTrue('false'))
+      assert(!isTrue(''))
+    })
+  })
+})
---
+++ packages/app-express/utils.ts
@@ -0,0 +1,72 @@
+// deno-lint-ignore-file no-explicit-any
+import { isHyperErr, R } from './deps.ts'
+
+import type { HttpResponse } from './types.ts'
+
+const { pick } = R
+
+interface HyperResponse {
+  ok: boolean
+  status?: number
+  msg?: string
+  [key: string]: any
+}
+
+export type Forkable<R = any, L = any> = {
+  fork: (left: (err: L) => any, right: (res: R) => any) => void
+}
+
+const sanitizeErr = pick(['ok', 'status', 'msg'])
+
+const isProduction = () => {
+  const env = Deno.env.get('DENO_ENV')
+  // Default to production behavior if no DENO_ENV is set
+  return !env || env === 'production'
+}
+
+/**
+ * See https://github.com/hyper63/hyper/issues/470
+ * for strategy
+ */
+export const fork = <R extends HyperResponse, L = any>(
+  res: HttpResponse,
+  code = 200,
+  m: Forkable<R, L>,
+) =>
+  m.fork(
+    (err) => {
+      console.log('fatal error received from core')
+      console.log(err)
+      res.status(500).send(isProduction() ? 'Internal Server Error' : err)
+    },
+    (serviceResult) => {
+      if (isHyperErr(serviceResult)) {
+        /**
+         * Use status in HyperError and fallback to 500
+         *
+         * Conditionally sanitize the results
+         */
+        return res
+          .status(serviceResult.status || 500)
+          .send(isProduction() ? sanitizeErr(serviceResult) : serviceResult)
+      }
+
+      res.status(code).send(serviceResult)
+    },
+  )
+
+export const isMultipartFormData = (contentType: string) => {
+  contentType = contentType || ''
+  return contentType.startsWith('multipart/form-data')
+}
+
+export const isFile = (path: string) => {
+  path = path || '/'
+  return (path.split('/').pop()?.indexOf('.') || 0) > -1
+}
+
+/**
+ * Add an empty string to coerce val to
+ * a string, then compare to string 'true'
+ */
+export const isTrue = (val: unknown) => (val + '').trim() === 'true'
