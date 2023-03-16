7f57d82c422eeaf76ceffbe88b93fe989051fd47
Tyler Hall <tyler@hyper.io>
2023-03-16 01:03:05 +0000

    fix(app-express): make json middleware more flexible #562

---
+++ packages/app-express/middleware/json.test.ts
@@ -0,0 +1,91 @@
+import { express } from '../deps.ts'
+import { assertObjectMatch } from '../dev_deps.ts'
+import { createHarness } from '../test-utils.ts'
+
+import { json } from './json.ts'
+
+const app = express()
+app.post('/foo/bar', json(), (req, res) => {
+  res.json(req.body)
+})
+
+Deno.test('json', async (t) => {
+  const harness = createHarness(app)
+
+  await t.step('should parse if application/json content-type', async () => {
+    await harness
+      .start()
+      .then(() =>
+        harness('/foo/bar', {
+          method: 'POST',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify({ id: '1', type: 'movie' }),
+        })
+          .then((res) => res.json())
+          .then((body) => {
+            assertObjectMatch(body, { id: '1', type: 'movie' })
+          })
+      )
+      .finally(async () => await harness.stop())
+  })
+
+  await t.step(
+    'should default parsing if no content-type is specified',
+    async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/foo/bar', {
+            method: 'POST',
+            body: JSON.stringify({ id: '1', type: 'movie' }),
+          })
+            .then((res) => res.json())
+            .then((body) => {
+              assertObjectMatch(body, { id: '1', type: 'movie' })
+            })
+        )
+        .finally(async () => await harness.stop())
+    },
+  )
+
+  await t.step('should respect the provided type', async (t) => {
+    const app = express()
+    app.post('/foo/bar', json({ type: 'foo/bar' }), (req, res) => {
+      res.json(req.body)
+    })
+    const harness = createHarness(app)
+
+    await t.step('should parse as json', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/foo/bar', {
+            method: 'POST',
+            headers: { 'content-type': 'foo/bar' },
+            body: JSON.stringify({ id: '1', type: 'movie' }),
+          })
+            .then((res) => res.json())
+            .then((body) => {
+              assertObjectMatch(body, { id: '1', type: 'movie' })
+            })
+        )
+        .finally(async () => await harness.stop())
+    })
+
+    await t.step('should parse as json', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/foo/bar', {
+            method: 'POST',
+            body: JSON.stringify({ id: '1', type: 'movie' }),
+          })
+            .then((res) => res.json())
+            .then((body) => {
+              assertObjectMatch({}, body)
+            })
+        )
+        .finally(async () => await harness.stop())
+    })
+  })
+})
--- packages/app-express/middleware/json.ts
+++ packages/app-express/middleware/json.ts
@@ -1,3 +1,16 @@
 import { express } from '../deps.ts'
 
-export const json = express.json
+/**
+ * Parses a requst body as JSON. Identical
+ * to the standard json() middleware with the distinction
+ * of setting a default catch-all content type option.
+ *
+ * Because we set type to a wildcard,
+ * This middleware will always parse the request body as JSON, regardless of
+ * content-type header.
+ *
+ * This is to take into account, badly behaved clients
+ * that don't provide the header explictly
+ */
+export const json = (options: Parameters<typeof express.json>[0] = {}) =>
+  express.json({ type: '*/*', ...options })
