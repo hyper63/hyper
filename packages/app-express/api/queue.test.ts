42c9cbfe4eab7af04a7094d244a1e363eb198255
Tyler Hall <tyler@hyper.io>
2023-04-06 15:16:30 +0000

    test(app-express): queue tests #556

--- packages/app-express/api/queue.test.ts
+++ packages/app-express/api/queue.test.ts
@@ -12,6 +12,26 @@ const services: any = {
         ok: true,
         arg,
       }),
+    delete: (arg: any) =>
+      crocks.Async.Resolved({
+        ok: true,
+        arg,
+      }),
+    post: (arg: any) =>
+      crocks.Async.Resolved({
+        ok: true,
+        arg,
+      }),
+    list: (arg: any) =>
+      crocks.Async.Resolved({
+        ok: true,
+        arg,
+      }),
+    cancel: (arg: any) =>
+      crocks.Async.Resolved({
+        ok: true,
+        arg,
+      }),
   },
 }
 
@@ -67,5 +87,157 @@ Deno.test('queue', async (t) => {
     })
   })
 
-  // TODO: add more test coverage here
+  await t.step('DELETE /queue/:name', async (t) => {
+    await t.step('should set the content-type header', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/queue/movies', {
+            method: 'DELETE',
+          }).then((res) => {
+            assertEquals(
+              res.headers.get('content-type'),
+              'application/json; charset=utf-8',
+            )
+            return res.body?.cancel()
+          })
+        )
+        .finally(async () => await harness.stop())
+    })
+
+    await t.step('should pass name route param to core', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/queue/movies', {
+            method: 'DELETE',
+          })
+            .then((res) => res.json())
+            .then((body) => {
+              assertEquals(body.arg, 'movies')
+            })
+        )
+        .finally(async () => await harness.stop())
+    })
+  })
+
+  await t.step('POST /queue/:name', async (t) => {
+    await t.step('should set the content-type header', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/queue/movies', {
+            method: 'POST',
+            body: JSON.stringify({
+              type: 'foo',
+              id: '1',
+            }),
+          }).then((res) => {
+            assertEquals(
+              res.headers.get('content-type'),
+              'application/json; charset=utf-8',
+            )
+            return res.body?.cancel()
+          })
+        )
+        .finally(async () => await harness.stop())
+    })
+
+    await t.step('should pass name route params and body to core', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/queue/movies', {
+            method: 'POST',
+            body: JSON.stringify({
+              type: 'foo',
+              id: '1',
+            }),
+          })
+            .then((res) => res.json())
+            .then((body) => {
+              assertObjectMatch(body.arg, {
+                name: 'movies',
+                job: {
+                  type: 'foo',
+                  id: '1',
+                },
+              })
+            })
+        )
+        .finally(async () => await harness.stop())
+    })
+  })
+
+  await t.step('GET /queue/:name', async (t) => {
+    await t.step('should set the content-type header', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/queue/movies?status=READY').then((res) => {
+            assertEquals(
+              res.headers.get('content-type'),
+              'application/json; charset=utf-8',
+            )
+            return res.body?.cancel()
+          })
+        )
+        .finally(async () => await harness.stop())
+    })
+
+    await t.step(
+      'should pass name route params and status query to core',
+      async () => {
+        await harness
+          .start()
+          .then(() =>
+            harness('/queue/movies?status=READY')
+              .then((res) => res.json())
+              .then((body) => {
+                assertObjectMatch(body.arg, {
+                  name: 'movies',
+                  status: 'READY',
+                })
+              })
+          )
+          .finally(async () => await harness.stop())
+      },
+    )
+  })
+
+  await t.step('POST /queue/:name/:id/_cancel', async (t) => {
+    await t.step('should set the content-type header', async () => {
+      await harness
+        .start()
+        .then(() =>
+          harness('/queue/movies/1/_cancel', { method: 'POST' }).then((res) => {
+            assertEquals(
+              res.headers.get('content-type'),
+              'application/json; charset=utf-8',
+            )
+            return res.body?.cancel()
+          })
+        )
+        .finally(async () => await harness.stop())
+    })
+
+    await t.step(
+      'should pass name route params and status query to core',
+      async () => {
+        await harness
+          .start()
+          .then(() =>
+            harness('/queue/movies/1/_cancel', { method: 'POST' })
+              .then((res) => res.json())
+              .then((body) => {
+                assertObjectMatch(body.arg, {
+                  name: 'movies',
+                  id: '1',
+                })
+              })
+          )
+          .finally(async () => await harness.stop())
+      },
+    )
+  })
 })
