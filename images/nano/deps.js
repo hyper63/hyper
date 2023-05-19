4c96a8a98843cc5a5fcc6e32ffe0b37d1f0b485b
Tyler Hall <tyler@hyper.io>
2023-05-19 18:35:57 -0400

    chore(nano): use newest deps and refactor tests and configs

--- images/nano/deps.js
+++ images/nano/deps.js
@@ -1,13 +1,13 @@
-export { default as hyper } from 'https://x.nest.land/hyper@3.4.2/mod.js'
-export { default as app } from 'https://x.nest.land/hyper-app-opine@2.3.0/mod.js'
+export { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.0.0/packages/core/mod.ts'
+export { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.0.1/packages/app-express/mod.ts'
 
 // adapters
-export { default as pouchdb } from 'https://x.nest.land/hyper-adapter-pouchdb@0.1.6/mod.js'
-export { default as cache } from 'https://x.nest.land/hyper-adapter-sqlite@2.0.3/mod.js'
-export { default as fs } from 'https://x.nest.land/hyper-adapter-fs@2.1.0/mod.js'
-export { default as minisearch } from 'https://x.nest.land/hyper-adapter-minisearch@2.0.0/mod.js'
-export { default as queue } from 'https://x.nest.land/hyper-adapter-queue@0.1.0/mod.js'
+export { default as pouchdb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-pouchdb/v0.1.6/mod.js'
+export { default as cache } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-sqlite/v2.0.9/mod.js'
+export { default as fs } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-fs/v3.0.0/mod.js'
+export { default as minisearch } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-minisearch/v2.1.0/mod.js'
+export { default as queue } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-queue/v0.1.0/mod.js'
 
-export { join } from 'https://deno.land/std@0.167.0/path/mod.ts'
-export { parse } from 'https://deno.land/std@0.167.0/flags/mod.ts'
-export * as Colors from 'https://deno.land/std@0.167.0/fmt/colors.ts'
+export { join } from 'https://deno.land/std@0.188.0/path/mod.ts'
+export { parse } from 'https://deno.land/std@0.188.0/flags/mod.ts'
+export * as Colors from 'https://deno.land/std@0.188.0/fmt/colors.ts'
---
+++ images/nano/main.test.js
@@ -0,0 +1,41 @@
+import { assertEquals } from 'https://deno.land/std@0.188.0/testing/asserts.ts'
+import { main } from './main.js'
+import { createHarness } from './test-utils.js'
+
+Deno.test({
+  name: 'GET /',
+  async fn() {
+    const app = await main({
+      middleware: [
+        (app) => app.get('/foobar', (_req, res) => res.json({ ok: true })),
+      ],
+    })
+
+    const harness = createHarness(app)
+
+    await harness.start()
+      .then(() =>
+        harness('/', { method: 'GET' }).then((res) => {
+          assertEquals(res.status, 200)
+          assertEquals(
+            res.headers.get('content-type'),
+            'application/json; charset=utf-8',
+          )
+          return res.body?.cancel()
+        })
+      )
+      .then(() =>
+        harness('/foobar', { method: 'GET' }).then((res) => {
+          assertEquals(res.status, 200)
+          assertEquals(
+            res.headers.get('content-type'),
+            'application/json; charset=utf-8',
+          )
+          return res.body?.cancel()
+        })
+      )
+      .finally(async () => await harness.stop())
+  },
+  sanitizeResources: false,
+  sanitizeOps: false,
+})
