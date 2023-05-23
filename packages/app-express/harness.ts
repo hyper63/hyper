0b326d761d53f510d834f3293ece9bd407886491
Tyler Hall <tyler@hyper.io>
2023-05-23 18:52:49 -0400

    refactor(app-express): remove nest.land imports

--- packages/app-express/harness.ts
+++ packages/app-express/harness.ts
@@ -1,8 +1,8 @@
 // Load .env
 import 'https://deno.land/std@0.182.0/dotenv/load.ts'
 
-import { default as core } from 'https://x.nest.land/hyper@3.4.2/mod.js'
-import namespacedS3 from 'https://x.nest.land/hyper-adapter-namespaced-s3@3.0.0/mod.js'
+import { default as core } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.0.0/packages/core/mod.ts'
+import namespacedS3 from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-namespaced-s3/v3.0.0/mod.js'
 
 import app from './mod.ts'
 
