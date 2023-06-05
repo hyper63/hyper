c713080d231bb258422f6baeee1925dc62a20ec0
Tyler Hall <tyler@hyper.io>
2023-06-05 13:12:05 -0400

    chore(nano): bump minisearch dep to latest

--- images/nano/deps.js
+++ images/nano/deps.js
@@ -4,7 +4,7 @@ export { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/
 // adapters
 export { default as sqlite } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-sqlite/v2.0.11/mod.js'
 export { default as fs } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-fs/v3.0.1/mod.js'
-export { default as minisearch } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-minisearch/v2.1.2/mod.js'
+export { default as minisearch } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-minisearch/v2.1.3/mod.js'
 export { default as hooks } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-hooks/v1.0.6/mod.js'
 export { default as pouchdb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-pouchdb/v0.1.7/mod.js'
 export { default as queue } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-queue/v0.1.1/mod.js'
