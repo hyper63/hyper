33e757e0397f8b7331347a161b99eea3191ba72a
Tyler Hall <tyler@hyper.io>
2023-11-20 12:05:04 -0500

    chore(nano): bump deps

--- images/nano/deps.js
+++ images/nano/deps.js
@@ -1,14 +1,14 @@
-export { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.0.2/packages/core/mod.ts'
-export { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.0.3/packages/app-express/mod.ts'
+export { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.0/packages/core/mod.ts'
+export { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.0/packages/app-express/mod.ts'
 
 // adapters
 export { default as sqlite } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-sqlite/v2.0.11/mod.js'
 export { default as fs } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-fs/v3.0.1/mod.js'
 export { default as minisearch } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-minisearch/v2.1.3/mod.js'
 export { default as hooks } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-hooks/v1.0.6/mod.js'
-export { default as pouchdb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-pouchdb/v0.1.7/mod.js'
-export { default as queue } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-queue/v0.1.1/mod.js'
+export { default as pouchdb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-pouchdb/v0.2.0/mod.js'
+export { default as queue } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-queue/v0.3.0/mod.js'
 
-export { join } from 'https://deno.land/std@0.188.0/path/mod.ts'
-export { parse } from 'https://deno.land/std@0.188.0/flags/mod.ts'
-export * as Colors from 'https://deno.land/std@0.188.0/fmt/colors.ts'
+export { join } from 'https://deno.land/std@0.207.0/path/mod.ts'
+export { parseArgs as parse } from 'https://deno.land/std@0.207.0/cli/mod.ts'
+export * as Colors from 'https://deno.land/std@0.207.0/fmt/colors.ts'
