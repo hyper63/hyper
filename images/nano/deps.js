825a6b7f6ba5334d84b680655115bdf904b4e3eb
Tyler Hall <tyler@hyper.io>
2024-08-19 20:55:24 +0000

    chore(nano): bump deps. Update MongoDB version 7.0.4 -> 7.0.11

--- images/nano/deps.js
+++ images/nano/deps.js
@@ -6,9 +6,9 @@ export { default as sqlite } from 'https://raw.githubusercontent.com/hyper63/hyp
 export { default as fs } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-fs/v3.0.2/mod.js'
 export { default as minisearch } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-minisearch/v2.1.4/mod.js'
 export { default as hooks } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-hooks/v1.0.6/mod.js'
-export { default as mongodb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-mongodb/v3.3.0/mod.ts'
+export { default as mongodb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-mongodb/v3.3.1/mod.ts'
 export { default as queue } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-queue/v0.3.1/mod.js'
 
-export { join } from 'https://deno.land/std@0.208.0/path/mod.ts'
-export { parseArgs as parse } from 'https://deno.land/std@0.208.0/cli/mod.ts'
-export * as Colors from 'https://deno.land/std@0.208.0/fmt/colors.ts'
+export { join } from 'https://deno.land/std@0.224.0/path/mod.ts'
+export { parseArgs as parse } from 'https://deno.land/std@0.224.0/cli/mod.ts'
+export * as Colors from 'https://deno.land/std@0.224.0/fmt/colors.ts'
--- images/nano/main.test.js
+++ images/nano/main.test.js
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.207.0/assert/mod.ts'
+import { assertEquals } from 'https://deno.land/std@0.224.0/assert/mod.ts'
 import { main } from './main.js'
 import { createHarness } from './test-utils.js'
 
